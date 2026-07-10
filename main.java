e4e4fb6a71e4effb341422744505d093019d58ee
DJRDhananjay <128344998+DJRDhananjay@users.noreply.github.com>
2026-07-10 15:42:34 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int count=0;
+		String name="dhananjay kumar ravi";
+		for(int i=0;i<name.length();i++){
+		    char ch=name.charAt(i);
+		    if(ch==' '){
+		        count=0;
+		    }else{
+		        if(count%2==0){
+		            if(ch>='a'&&ch<='z'){
+		                ch=(char)(ch-32);
+		            }
+		        }
+		        else{
+		            if(ch>='A'&& ch<='Z'){
+		                ch=(char)(ch+32);
+		            }
+		        }
+		        System.out.print(ch);
+		        count++;
+		    }
+		}
+		
+	    
+	}
+}
